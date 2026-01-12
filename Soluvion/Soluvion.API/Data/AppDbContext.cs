2eb99fd8500b6db087862ac0e1d42670eac68581
tsvandra <tsvandra@ccn.sk>
2026-01-12 13:58:21 +0100

    adatbazis kapcsolat letrehozasa

---
+++ Soluvion/Soluvion.API/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Soluvion.API.Models;
+
+namespace Soluvion.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Company> Companies{ get; set; }
+    }
+}
