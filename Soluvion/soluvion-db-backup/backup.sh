cc499ffa48775b193cb4897d9edaf5b14f7d9d53
tsvandra <tsvandra@ccn.sk>
2026-02-23 22:14:15 +0100

    dockerfile es backup.sh

---
+++ Soluvion/soluvion-db-backup/backup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Pontos dátum és időpont generálása a fájlnévhez
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+BACKUP_FILE="skanisalon_backup_${TIMESTAMP}.sql"
+
+echo "Mentés indítása: $BACKUP_FILE"
+# Letölti az adatbázist a megadott URL-ről
+pg_dump $DB_URL > $BACKUP_FILE
+
+echo "Feltöltés a Backblaze B2-re..."
+# Átküldi a fájlt a Backblaze tárolóba a megadott kulcsokkal
+aws s3 cp $BACKUP_FILE s3://$S3_BUCKET/$BACKUP_FILE --endpoint-url https://$S3_ENDPOINT
+
+echo "Kész! Takarítás..."
+# Letörli a fájlt a kis robot memóriájából, hogy ne foglalja a helyet
+rm $BACKUP_FILE
+
+echo "A mentés sikeresen befejeződött!"
